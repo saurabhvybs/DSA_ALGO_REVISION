d3b6dbbd7efc7d3976cee3e8a67bed6a343a8125
saurabhvybs <psaurabh51@gmail.com>
2025-04-23 04:29:15 +0530

    Bubble Sort

---
+++ Bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bubble_sort(int *arr, int size){
+    for(int j = size; j>0 ; j--){
+// this will remains false if array is already sorted as there will be no swapping happens in first iteration which tells us that array is already sorted
+        bool swapped = false;
+        for(int k = 0; k < j-1; k++){
+            if(arr[k]>arr[k+1]){
+                swap(arr[k],arr[k+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped)break;
+    }
+}
+int main(){
+    cout<<"Enter the size of the array"<<endl;
+    int size;
+    cin>>size;
+    cout<<"Enter the elements in the array"<<endl;
+    int *arr = new int[size];
+    for(int i=0; i < size; i++){
+        cin>>arr[i];
+    }
+    bubble_sort(arr,size);
+    for(int i=0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+    delete [] arr;
+    return 0;
+}
