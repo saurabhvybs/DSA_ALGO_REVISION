8e56dc0322e03d78f5dbfa0003d73a4abeb10ce2
SAURABH PANDEY <146021671+saurabhvybs@users.noreply.github.com>
2025-04-27 01:50:28 +0530

    Update Selection_sort.cpp

--- Selection_sort.cpp
+++ Selection_sort.cpp
@@ -1,4 +1,6 @@
 // Online C++ compiler to run C++ program online
+//Basically the idea here is to iterate in the array everytime and just find the minimum element in each iteration and swap it with the element in front.
+// Time complexity thus here goes to O(n^2).
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
